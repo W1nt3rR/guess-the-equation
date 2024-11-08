8a48e4b2a274698741a9ecfe0cbe6efeb8ab2f1f
W1nt3rR <frostywinterr@gmail.com>
2024-11-08 22:53:12 +0100

    Add GitHub Actions workflow for deploying Vite app to GitHub Pages and update Vite config for production base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/w1nt3rr.github.io/guess-the-equation/' : '/',
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
